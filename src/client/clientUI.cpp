523b83d5797b4b413e234865fd6b3452bb42019a
player0-glitch <mokhaditlhalefo@gmail.com>
2025-02-20 17:10:13 +0200

    feat: messages windows now has a default size

--- src/client/clientUI.cpp
+++ src/client/clientUI.cpp
@@ -16,8 +16,9 @@ using std::vector;
 
 std::vector<std::string> online_user = {"<Client0>", "<Client1>", "<Client2>"};
 std::vector<std::string> user_messages = {"<Client0>: wagwan",
-                                          "<Client1>: ain none gang!"};
-
+                                          "<Client1>: ain none gang!",
+                                          "<Client0>: Aight say less then"};
+// auto create_messages_window(vector<std::string> &user_messages) -> Component;
 int main() {
   ScreenInteractive screen = ScreenInteractive::TerminalOutput();
 
@@ -61,7 +62,7 @@ int main() {
       input_len = input_text.length();
       if (!(input_len > 0))
         return false;
-      user_messages.push_back("<You>: " + input_text +
+      user_messages.push_back("<You>: " + input_text + " " +
                               std::to_string(input_len));
       input_text.clear();
       return true;
@@ -82,8 +83,10 @@ int main() {
 
   auto window_layout = Container::Vertical(
       {Container::Horizontal(
-           {online_panel | size(WIDTH, GREATER_THAN, online_panel_sizex),
-            message_window | size(WIDTH, GREATER_THAN, message_window_sizex)}),
+           {online_panel | size(WIDTH, GREATER_THAN, online_panel_sizex) |
+                size(HEIGHT, GREATER_THAN, 10),
+            message_window | size(WIDTH, GREATER_THAN, message_window_sizex) |
+                size(HEIGHT, GREATER_THAN, 12)}),
        input_box});
 
   auto debug = [&]() -> Component {
@@ -96,3 +99,37 @@ int main() {
   screen.Loop(debug());
   return EXIT_SUCCESS;
 }
+
+/*@brief method used to create scrollable message window area
+ *@param user_messages is a vector of all the user messages coming and going
+ * to the server
+auto create_messages_window(vector<std::string> &user_messages) -> Component {
+  vector<Component> message_components;
+  for (const auto &msg : user_messages)
+
+    // Create a vertical Container to be able to make it scrollable
+    auto online_panel = Renderer([] {
+      vector<Element> user_lines;
+      for (const auto &u : online_user)
+        user_lines.push_back(paragraphAlignLeft(u));
+
+      return window(text("Online") | hcenter,
+                    vbox(user_lines) | frame | size(WIDTH, GREATER_THAN, 20));
+    });
+
+  auto container =
+      Container::Vertical({Container::Horizontal({message_components})});
+   auto container = middle;
+   container = ResizableSplitLeft(left, container, &left_size);
+   container = ResizableSplitRight(right, container, &right_size);
+   container = ResizableSplitTop(top, container, &top_size);
+   container = ResizableSplitBottom(bottom, container, &bottom_size);
+
+   auto renderer =
+       Renderer(container, [&] { return container->Render() | border; });
+   */
+// Making the online panel for users to see who is connected to chat server
+/*
+  container |= vscroll_indicator;
+  return container;
+}*/
